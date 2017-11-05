5968a46d1d60aa6741908c6886efb68c9d8ac006
Ricardo Proença <ricproenca@gmail.com>
2017-11-05 02:20:17 +0000

    added: more security

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import cors from "cors";
+import RateLimit from "express-rate-limit";
 import websocket from "ws";
 import dateFormat from "dateformat";
 import initializeDb from "./db";
@@ -18,8 +19,31 @@ app.set("port", process.env.PORT || config.port);
 // Configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(helmet());
+
+// Enable CORS to allow cross-origin HTTP requests from a different domain
 app.use(cors());
+
+// Helmet default config:
+// dnsPrefetchControl: controls browser DNS prefetching
+// frameguard: to prevent clickjacking
+// hidePoweredBy: to remove the X-Powered-By header
+// hsts: for HTTP Strict Transport Security
+// ieNoOpen: sets X-Download-Options for IE8+
+// noSniff: to keep clients from sniffing the MIME type
+// xssFilter: adds some small XSS protections
+app.use(helmet({ frameguard: { action: "sameorigin" } }));
+
+// only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+//app.enable("trust proxy");
+
+// Protect from Brute Force and DDOS Attacks
+app.use(
+  new RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    delayMs: 0 // disable delaying - full speed until the max limit is reached
+  })
+);
 
 console.log("### Online Post-It Board Server ###");
 
