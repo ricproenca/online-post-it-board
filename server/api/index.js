4b6a2bc4d57ed1705283c1e62cc6ef4020760666
Ricardo Proenca <ricproenca@gmail.com>
2017-11-06 10:49:56 +0000

    added: api with db  conn

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,11 @@ import { version } from "../../package.json";
 import { Router } from "express";
 import Notes from "./notes";
 
-export default (notes, db, broadcast) => {
+export default (db, broadcast) => {
   let api = Router();
 
   // mount the notes resource
-  api.use("/notes", Notes(notes, db, broadcast));
+  api.use("/notes", Notes(db, broadcast));
 
   // perhaps expose some API metadata at the root
   api.get("/", (req, res) => {
--- server/api/notes.js
+++ server/api/notes.js
@@ -1,5 +1,6 @@
 import resource from "resource-router-middleware";
 import uuidv1 from "uuid/v1";
+import dateFormat from "dateformat";
 
 import config from "../config.json";
 
@@ -28,7 +29,7 @@ const validateNote = note => {
   return true;
 };
 
-export default (notes, db, broadcast) =>
+export default (db, broadcast) =>
   resource({
     /** Property name to store preloaded entity on `request`. */
     id: "note",
@@ -37,21 +38,42 @@ export default (notes, db, broadcast) =>
 	 *  Errors terminate the request, success sets `req[id] = data`.
 	 */
     load(req, id, callback) {
-      let note = notes.find(note => note.id === id),
-        err = note ? null : "Not found";
-      callback(err, note);
+      db
+        .collection("notes")
+        .find()
+        .toArray(function(err, results) {
+          if (err) {
+            res.sendStatus(500);
+            return;
+          }
+          let note = results.find(note => note.id === id);
+          const notFoundMessage = note ? null : "Not found";
+          callback(notFoundMessage, note);
+        });
     },
 
     /** GET / - List all entities */
     index({ params }, res) {
-      console.log("API request list all");
-      res.json(notes);
-      broadcast(notes);
+      console.log(
+        `${dateFormat(null, "isoUtcDateTime")} - API REQUEST get all`
+      );
+
+      db
+        .collection("notes")
+        .find()
+        .toArray(function(err, results) {
+          if (err) {
+            res.sendStatus(500);
+            return;
+          }
+          res.json(results); // should respond?
+          broadcast(results);
+        });
     },
 
     /** POST / - Create a new entity */
     create({ body }, res) {
-      console.log("API request create");
+      console.log(`${dateFormat(null, "isoUtcDateTime")} - REQUEST create`);
 
       if (!validateNote(body)) {
         res.sendStatus(400);
@@ -59,49 +81,86 @@ export default (notes, db, broadcast) =>
       }
 
       body.id = uuidv1();
+      body._id = uuidv1();
       body.tags = []
         .concat(getTagsInText(body.title))
         .concat(getTagsInText(body.description));
 
-      notes.push(body);
-      res.sendStatus(204);
-      broadcast(notes);
+      db.collection("notes").save(body, (err, result) => {
+        if (err) {
+          return console.log(err);
+        }
+        res.sendStatus(204);
+
+        db
+          .collection("notes")
+          .find()
+          .toArray(function(err, results) {
+            broadcast(results);
+          });
+      });
     },
 
     /** GET /:id - Return a given entity */
     read({ note }, res) {
-      console.log(`API request read ${note.id}`);
-      res.json(note);
-      broadcast(notes);
+      console.log(
+        `${dateFormat(null, "isoUtcDateTime")} - REQUEST read [${note.id}]`
+      );
+      res.json(results);
+      broadcast(results);
     },
 
     /** PUT /:id - Update a given entity */
     update({ note, body }, res) {
-      console.log(`API request update ${note.id}`);
+      console.log(
+        `${dateFormat(null, "isoUtcDateTime")} - REQUEST update [${note.id}]`
+      );
 
       if (!validateNote(body)) {
         res.sendStatus(400);
         return;
       }
 
+      body.id = note.id;
       body.tags = []
         .concat(getTagsInText(body.title))
         .concat(getTagsInText(body.description));
 
-      for (let key in body) {
-        if (key !== "id") {
-          note[key] = body[key];
-        }
-      }
+      db
+        .collection("notes")
+        .updateOne({ id: body.id }, { $set: body }, (err, result) => {
+          if (err) {
+            return console.log(err);
+          }
+          res.sendStatus(204);
 
-      res.sendStatus(204);
-      broadcast(notes);
+          db
+            .collection("notes")
+            .find()
+            .toArray(function(err, results) {
+              broadcast(results);
+            });
+        });
     },
 
     /** DELETE /:id - Delete a given entity */
     delete({ note }, res) {
-      console.log(`API request delete ${note.id}`);
-      notes.splice(notes.indexOf(note), 1);
-      broadcast(notes);
+      console.log(
+        `${dateFormat(null, "isoUtcDateTime")} - REQUEST delete [${note.id}]`
+      );
+
+      db.collection("notes").deleteOne({ id: note.id }, (err, result) => {
+        if (err) {
+          return console.log(err);
+        }
+        res.sendStatus(204);
+
+        db
+          .collection("notes")
+          .find()
+          .toArray(function(err, results) {
+            broadcast(results);
+          });
+      });
     }
   });
